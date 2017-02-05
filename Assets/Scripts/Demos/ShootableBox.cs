72d5b545d5cad8e62ca66c94e9b20a8a22c475e0
Brandon Jones <bcjones1@fullsail.edu>
2017-02-05 16:03:48 -0500

    Initial Commit
    Take 2; again.

---
+++ Assets/Scripts/Demos/ShootableBox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootableBox : MonoBehaviour {
+
+	//The box's current health point total
+	public int currentHealth = 3;
+
+	public void Damage(int damageAmount)
+	{
+		//subtract damage amount when Damage function is called
+		currentHealth -= damageAmount;
+        Debug.Log("I'm being shot!");
+
+		//Check if health has fallen below zero
+		if (currentHealth <= 0) 
+		{
+			//if health has fallen below zero, deactivate it 
+			gameObject.SetActive (false);
+            Debug.Log("I'm dead!");
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerHealthAmy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealthAmy : MonoBehaviour {
+
+    public int baseHealth = 125;
+    public int currentHealth;
+    public Slider HealthSlider;
+    public Image damageImage;
+    public AudioClip deathSound;
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement AmyControlScript;
+    bool isDead;
+    bool Damaged;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
