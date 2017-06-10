db80db080c97acb13c346ea0c75c509ac31c8a68
Brandon Jones <bcjones1@fullsail.edu>
2017-06-10 18:34:25 -0400

    :circus_tent: Added .gitattributes & .gitignore files

--- Assets/Scripts/Messages/ICustomMessageTarget.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine.EventSystems;
-
-public interface ICustomMessageTarget : IEventSystemHandler
-{
-    // functions that can be called via the messaging system
-    void Message1();
-    void Message2();
-}
