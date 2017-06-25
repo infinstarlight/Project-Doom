c11603f9a5f9d8f43ca1688a48e91bb6d4310b66
Brandon Jones <bcjones1@fullsail.edu>
2017-06-25 18:07:28 -0400

    Trying something nwq

---
+++ Assets/Scripts/JackAnimScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackAnimScript : MonoBehaviour {
+
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+        myAnimator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
+        if(Input.GetButtonDown("Jump"))
+        {
+            myAnimator.SetBool("isJumping", true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour {
+
+    public int numSelector;
+    public GameObject[] enemies;
+    public GameObject EnemySelector;
+    public Transform[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+
+        waypoints = new Transform[transform.childCount];
+        int i = 0;
+
+        foreach (Transform t in transform)
+        {
+            waypoints[i++] = t;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.tag == "Player")
+        {
+            GameObject enemieSpawn = Instantiate(EnemySelector, transform.parent, true);
+        }
+    }
+}
